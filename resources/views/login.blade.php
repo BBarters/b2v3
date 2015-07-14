a9a56847ba3b00ba8bb33ccedd440d3b3e8ee5c6
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2015-07-14 15:35:39 +0530

    [#98894308] created a parent html and login html
    The parent html contains the script link and style links which can be
    yielded

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+@extends('parent.parent')
+
+
+@section('content')
+
+
+<div class="container">
+
+       <div class="rows">
+
+           <div class="col-xs-6 col-xs-offset-3">
+
+               <div class="well">
+
+               </div>
+
+           </div>
+
+       </div>
+     </div>
+
+
+@stop
