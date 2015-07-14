97508f49facf31a963b8b08bce74545d4f686be8
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2015-07-14 22:46:12 +0530

    [#98894308]  a message dialog
    just a test

---
+++ resources/views/parent/message.blade.php
@@ -0,0 +1,17 @@
+
+
+<!--somethings usefull-->
+
+<div id="complete-dialog" class="modal fade" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="message-title"></h4>
+            </div>
+            <div class="modal-body" style="margin-top: 26px">
+                <p id="message-body"></p>
+            </div>
+        </div>
+    </div>
+</div>
