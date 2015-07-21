935dcfc7393e677f1b824b678571da50764ab754
Riyaz Ahmed <ahmed.riyaz94@gmail.com>
2015-07-21 21:07:32 +0530

    [#98894308] function added to loginTest to check both condition

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -6,13 +6,33 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LoginTest extends TestCase
 {
-    public function test_login()
+    public function test_login_true()
     {
         Session::start();
-        $this->call('POST','/login',array('username'=>'riyaz942','password'=>'riyaz','_token'=>csrf_token()));
+
+        $credentials = array(
+        'username'=>'riyaz942',
+        'password'=>'riyaz',
+        '_token'=>csrf_token());
+
+        $this->call('POST','/login',$credentials);
 
         $this->assertRedirectedTo('home');
         //$this->assertEquals($content,'ok');
        // $this->assertTrue(true);
+    }
+
+    public function test_login_false()
+    {
+        Session::start();
+
+        $credentials = array(
+            'username'=>'incorrect_username',
+            'password'=>'incorrect_password',
+            '_token'=>csrf_token());
+
+        $this->call('POST','/login',$credentials);
+
+        $this->assertRedirectedTo('/');
     }
 }
