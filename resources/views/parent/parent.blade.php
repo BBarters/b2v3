a329741c7ecaa3c0735441d0f0ed208a5cfb008b
prateekkish <prateekkish@gmail.com>
2015-07-24 22:09:32 +0530

    [#98894308]

--- resources/views/parent/parent.blade.php
+++ resources/views/parent/parent.blade.php
@@ -3,21 +3,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
     -->
-    <link href="http:\\localhost\b2v3\vendor\phpunit\php-code-coverage\src\CodeCoverage\Report\HTML\Renderer\Template\css\bootstrap.min.css" rel="stylesheet">
-    <link href="http://localhost/b2v3/bower_components/bootstrap-material-design/dist/css/roboto.min.css" rel="stylesheet">
-    <link href="http://localhost/b2v3/bower_components/bootstrap-material-design/dist/css/material-fullpalette.min.css" rel="stylesheet">
-    <link href="http://localhost/b2v3/bower_components/bootstrap-material-design/dist/css/ripples.min.css" rel="stylesheet">
-    <link href="http://localhost/b2v3/bower_components/snackbarjs/dist/snackbar.min.css" rel="stylesheet">
+    <link href="http:\\localhost:8080\b2v3\vendor\phpunit\php-code-coverage\src\CodeCoverage\Report\HTML\Renderer\Template\css\bootstrap.min.css" rel="stylesheet">
+    <link href="http://localhost:8080/b2v3/bower_components/bootstrap-material-design/dist/css/roboto.min.css" rel="stylesheet">
+    <link href="http://localhost:8080/b2v3/bower_components/bootstrap-material-design/dist/css/material-fullpalette.min.css" rel="stylesheet">
+    <link href="http://localhost:8080/b2v3/bower_components/bootstrap-material-design/dist/css/ripples.min.css" rel="stylesheet">
+    <link href="http://localhost:8080/b2v3/bower_components/snackbarjs/dist/snackbar.min.css" rel="stylesheet">
     <!--<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/material-fullpalette.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/material.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/ripples.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/roboto.css" rel="stylesheet">
     <link href="//cdn.rawgit.com/FezVrasta/dropdown.js/master/jquery.dropdown.css" rel="stylesheet">
     <script src="//code.jquery.com/jquery-1.10.2.js"></script>-->
-    <script src="http://localhost/b2v3/bower_components/jquery/dist/jquery.js"></script>
-    <script src="http://localhost/b2v3/js/script.js"></script>
+    <script src="http://localhost:8080/b2v3/bower_components/jquery/dist/jquery.js"></script>
+    <script src="http://localhost:8080/b2v3/js/script.js"></script>
 
-    <link href="http://localhost/b2v3/css/style.css" rel="stylesheet">
+    <link href="http://localhost:8080/b2v3/css/style.css" rel="stylesheet">
 </head>
 
      <body>
@@ -27,10 +27,10 @@
 
 
      <!--<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.js"></script>-->
-     <script src="http:\\localhost\b2v3\vendor\phpunit\php-code-coverage\src\CodeCoverage\Report\HTML\Renderer\Template\js\bootstrap.min.js"></script>
-     <script src="http://localhost/b2v3/bower_components/bootstrap-material-design/dist/js/material.js"></script>
-     <script src="http://localhost/b2v3/bower_components/bootstrap-material-design/dist/js/ripples.js"></script>
-     <script src="http://localhost/b2v3/bower_components/snackbarjs/dist/snackbar.min.js"></script>
+     <script src="http:\\localhost:8080\b2v3\vendor\phpunit\php-code-coverage\src\CodeCoverage\Report\HTML\Renderer\Template\js\bootstrap.min.js"></script>
+     <script src="http://localhost:8080/b2v3/bower_components/bootstrap-material-design/dist/js/material.js"></script>
+     <script src="http://localhost:8080/b2v3/bower_components/bootstrap-material-design/dist/js/ripples.js"></script>
+     <script src="http://localhost:8080/b2v3/bower_components/snackbarjs/dist/snackbar.min.js"></script>
      <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/material.js"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/ripples.js"></script>-->
 
